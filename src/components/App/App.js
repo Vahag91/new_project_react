d0fa3a1042da002bd7c1415df390b79473143b82
Vahagn Gevorgyan <gevorgyanvahag45@gmail.com>
2023-09-29 17:45:46 +0400

    updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,44 +1,12 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import Header from "../Header";
 
 function App() {
 
   return(
+<div>
+  <Header/>
+</div>
 
-    <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
-    <Container fluid>
-      <Navbar.Brand href="#">BOOTSTRAP</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
-        <Nav
-          className="me-auto my-2 my-lg-0"
-          style={{ maxHeight: '100px' }}
-          navbarScroll
-        >
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Contacts</Nav.Link>
-          <Nav.Link href="#">Info</Nav.Link>
-          <Nav.Link href="#">About us</Nav.Link>
-          <Nav.Link href="#">Pricing</Nav.Link>
-        </Nav>
-        <Form className="d-flex">
-          <Form.Control
-            type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-          />
-          <Button variant="outline-success">Search</Button>
-          <Button variant="outline-primary">Login</Button>
-          <Button variant="outline-danger">Logout</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
 );
 
   
---
+++ src/components/Header/Header.js
@@ -0,0 +1,44 @@
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+export default function Header(){
+
+
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+        <Container fluid>
+          <Navbar.Brand href="#">BOOTSTRAP</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Contacts</Nav.Link>
+              <Nav.Link href="#">Info</Nav.Link>
+              <Nav.Link href="#">About us</Nav.Link>
+              <Nav.Link href="#">Pricing</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="m-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success" className='m-2'>Search</Button>
+              <Button variant="primary" className='m-2'>Login</Button>
+              <Button variant="outline-danger" className='m-2'>Logout</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+}
